136160af63263b81af5812f3593bc41a0d16cfce
vandemjh <jack.vandem@gmail.com>
2020-07-25 13:54:28 -0400

    drafting environment vars

--- db/access.js
+++ db/access.js
@@ -1,5 +1,15 @@
-const {Pool} = require("pg");
+const { Pool } = require('pg');
 const pool = new Pool();
 module.exports = {
-	query: (text, params) => pool.query(text, params)
-}
+  query: (text, params) => pool.query(text, params),
+};
+
+// {
+// 	user: process.env.USER,
+// 	password: process.env.PASSWORD,
+// 	port: process.env.DB_PORT || 5432,
+// 	host: process.env.HOST || "localhost",
+// 	max: process.env.MAX_CLIENTS || 10,
+// 	idleTimeoutMillis: 30000,
+// 	connectionTimeoutMillis: 2000,
+// }
