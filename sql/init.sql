51fc0ff7a1dbffc732cc3e5ad8116f87e7fee7f3
vandemjh <jack.vandem@gmail.com>
2020-07-31 16:44:39 -0400

    updated sql init script

--- sql/init.sql
+++ sql/init.sql
@@ -3,32 +3,36 @@ DROP TABLE IF EXISTS posts CASCADE;
 DROP TABLE IF EXISTS comments CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS likes CASCADE;
+SET timezone = 'America/New_York';
 
 CREATE TABLE posts
 (
     pid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    comments UUID DEFAULT gen_random_uuid(),
-    likes UUID DEFAULT gen_random_uuid(),
-    poster UUID
+    description VARCHAR(10000) DEFAULT null,
+    location VARCHAR(1000) DEFAULT null,
+    imageurl VARCHAR(1000) DEFAULT null,
+    cid UUID DEFAULT gen_random_uuid(),
+    lid UUID DEFAULT gen_random_uuid(),
+    uid UUID NOT NULL
 );
 
 CREATE TABLE comments
 (
     cid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    comment VARCHAR(10000),
-    commentor UUID
+    comment VARCHAR(100000),
+    commenter UUID
 );
 
 CREATE TABLE users
 (
     uid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    username VARCHAR(100),
-    firstname VARCHAR(100),
-    lastname VARCHAR(100),
-    profilepictureurl varchar(100)
+    username VARCHAR(1000),
+    firstname VARCHAR(1000) DEFAULT NULL,
+    lastname VARCHAR(1000) DEFAULT NULL,
+    profilepictureurl varchar(1000) DEFAULT NULL
 );
 
 CREATE TABLE likes
