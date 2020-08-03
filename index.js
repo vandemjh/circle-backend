2d12007bca7c077b1720445c05ad6eacbb229b6f
vandemjh <jack.vandem@gmail.com>
2020-08-03 19:47:09 -0400

    updated process env startup flow

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const router = require('express-promise-router');
 const mountRoutes = require('./router');
 const fs = require('fs');
 const db = require('./db/access');
-const authMiddleware = require('./auth/auth');
+// const authMiddleware = require('./auth/auth');
 // const authSession = require('./auth/session');
 
 const APP = express();
@@ -18,28 +18,30 @@ if (process.env.STARTUP === 'true')
     if (err) {
       return console.log(err);
     }
-    db.query(sql.replace(/[\n\r]/g, '').replace(/    /g, ''));
+    db.query(sql.replace(/[\n\r]/g, '').replace(/    /g, '')).then(() => {
+      if (process.env.TEST_POSTS === 'true') {
+        // const sleep = (fn, m) => new Promise(() => setTimeout(fn, m));
+        db.query(`INSERT INTO users(username) VALUES('${'asdf'}') RETURNING uid`)
+          .then(
+            (success) => {
+              console.log('User created with uid: ' + success.rows[0].uid);
+              for (let i = 0; i < 100; i++) {
+                // setTimeout(() => {
+                db.query(
+                  `INSERT INTO posts(description, location, uid) VALUES('${i}', '${i}', '${success.rows[0].uid}')`
+                );
+                // }, 100)
+                // .then(() => console.log('Post added!'));
+              }
+              console.log('Done adding posts!');
+            },
+            (reject) => console.log(reject)
+          )
+          .catch((err) => console.log(err));
+      }
+    });
   });
 
-if (process.env.TEST_POSTS === 'true') {
-  // const sleep = (fn, m) => new Promise(() => setTimeout(fn, m));
-  db.query(`INSERT INTO users(username) VALUES('${'asdf'}') RETURNING uid`)
-    .then(
-      (success) => {
-        console.log('User created with uid: ' + success.rows[0].uid);
-        for (let i = 0; i < 100; i++) {
-          // setTimeout(() => {
-          db.query(
-            `INSERT INTO posts(description, location, uid) VALUES('${i}', '${i}', '${success.rows[0].uid}')`
-          );
-          // }, 100)
-          // .then(() => console.log('Post added!'));
-        }
-        console.log('Done adding posts!');
-      },
-      (reject) => console.log(reject)
-    )
-    .catch((err) => console.log(err));
-}
+
 
 APP.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
