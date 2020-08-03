a11904d206673ce2ff6cad8b01ab4accc581d866
vandemjh <jack.vandem@gmail.com>
2020-08-03 09:40:25 -0400

    trying another way

--- auth/auth.js
+++ auth/auth.js
@@ -1,26 +1,39 @@
-const OktaJwtVerifier = require('@okta/jwt-verifier')
+const session = require('express-session');
+const { ExpressOIDC } = require('@okta/oidc-middleware');
+const crypto = require('crypto');
+const base64url = require('base64url');
+const Router = require('express-promise-router');
+const router = new Router();
 
-const oktaJwtVerifier = new OktaJwtVerifier({
+var code_verifier = process.env.CODE_VERIFIER;
+var hash = crypto.createHash('sha256').update(code_verifier).digest();
+var CODE_CHALLENGE = base64url.encode(hash);
+
+module.exports = router;
+
+const oidc = new ExpressOIDC({
   issuer: process.env.ISSUER,
-})
-// clientId: process.env.CLIENT_ID
+  client_id: process.env.CLIENT_ID,
+  client_secret: CODE_CHALLENGE,
+  appBaseUrl: process.env.APP_BASE_URL,
+  redirect_uri: process.env.REDIRECT_URI,
+  scope: process.env.SCOPE,
+});
 
-module.exports = async (req, res, next) => {
-  try {
-    const { authorization } = req.headers
-    if (!authorization) {
-      throw new Error('You must send an Authorization header')
-    }
+router.use(
+  session({
+    secret: CODE_CHALLENGE,
+    resave: true,
+    saveUninitialized: false,
+  })
+  );
+  
+router.use(oidc.router);
 
-    const [authType, token] = authorization.trim().split(' ')
-    if (authType !== 'Bearer') throw new Error('Expected a Bearer token')
+router.get('/protected', oidc.ensureAuthenticated(), (req, res) => {
+  res.send("success!!");
+});
 
-    const { claims } = await oktaJwtVerifier.verifyAccessToken(token)
-    if (!claims.scp.includes(process.env.SCOPE)) {
-      throw new Error('Could not verify the proper scope')
-    }
-    next()
-  } catch (error) {
-    next(error.message)
-  }
-}
+router.get('/implicit/callback', oidc.ensureAuthenticated(), (req, res) => {
+  res.send("implicit-callback success!!");
+});
