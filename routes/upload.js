512d982ed5ed742769f052fe32c79eb34fd63519
vandemjh <jack.vandem@gmail.com>
2020-08-10 17:16:53 -0400

    fixed uploads, added upload folder

--- routes/upload.js
+++ routes/upload.js
@@ -1,23 +1,32 @@
 const Router = require('express-promise-router');
 const fileUpload = require('express-fileupload');
-const uuid = require('uuid');
+const { v4: uuid } = require('uuid');
 const router = new Router();
 
 module.exports = router;
 router.use(
   fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 },
+    debug: process.env.DEBUG,
   })
 );
 
-router.post('/upload', function (req, res) {
-  if (!req.file || Object.keys(req.file).length === 0) {
-    return res.status(400).send('No files were uploaded.');
-  }
-  if (req.file.foo.truncated) return res.status(413).send('File too large');
+router.post('/', function (req, res) {
+  try {
+    console.log(req.files)
+    var image = req.files.image;
+    const type = req.files.image.mimetype.split("/")[1];
+    if (!req.files || !image)
+      return res.status(400).send({ error: 'No files were uploaded.' });
 
-  req.file.file.mv(`./data/${uuid()}`, (err) => {
-    if (err) return res.status(500).send('Error uploading file');
-    res.send('File uploaded!');
-  });
+    if (image.truncated)
+      return res.status(413).send({ error: 'File too large' });
+    var link = `uploads/${uuid()}.${type}`;
+    image.mv(`./${link}`, (err) => {
+      if (err) return res.status(500).send({ error: 'Error uploading file' });
+    });
+    res.send({payload: link});
+  } catch (err) {
+    console.log(err);
+  }
 });
