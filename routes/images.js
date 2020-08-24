1c49cd6ce2823734093e24966b8f8f7c59692d6f
vandemjh <jack.vandem@gmail.com>
2020-08-24 15:52:49 -0400

    now serving binary straight from the database, eventually going to be combining images and uploads routes

--- routes/images.js
+++ routes/images.js
@@ -1,7 +1,29 @@
 const Router = require('express-promise-router');
+const db = require('../db/access');
+const btoa = require('btoa');
+const fs = require('fs');
 const router = new Router();
-
 const express = require('express');
 
-router.use('/', express.static('./data'));
 module.exports = router;
+
+router.use('/data', express.static('./data'));
+
+router.get('/:iid', async (req, res) => {
+  const iid = req.params.iid;
+  const result = await db.query(`SELECT image FROM images WHERE iid = $1`, [
+    iid,
+  ]);
+  //   console.log(result)
+  if (!result || result.rows.length == 0)
+    return res.status(404).send({ error: 'No such image.' });
+  const image = result.rows[0].image;
+  //   var binary = btoa(String.fromCharCode.apply(null, new Uint8Array(image)));
+
+//   console.log(image);
+//   fs.writeFile('img', image, 'base64', (err) => console.log(err));
+
+  return res.send(image);
+  //   console.log(image);
+  res.send(image);
+});
--- routes/upload.js
+++ routes/upload.js
@@ -1,5 +1,6 @@
 const Router = require('express-promise-router');
 const fileUpload = require('express-fileupload');
+const db = require('../db/access');
 const { v4: uuid } = require('uuid');
 const router = new Router();
 
@@ -7,24 +8,31 @@ module.exports = router;
 router.use(
   fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 },
-    debug: process.env.DEBUG,
+    debug: process.env.DEBUG === 'true' && process.env.SKIP_LOGGING !== 'true',
   })
 );
 
-router.post('/', function (req, res) {
+router.post('/', async (req, res) => {
   try {
-    console.log(req.files);
+    if (process.env.SKIP_LOGGING !== 'true') console.log(req.files);
     var image = req.files.image;
     const type = req.files.image.mimetype.split('/')[1];
-    if (!req.files || !image)
+    const data = req.files.image.data;
+    if (!req.files || !image || !data)
       return res.status(400).send({ error: 'No files were uploaded.' });
-
+    if (!type) return res.status(415).send({ error: 'Mimetype error.' });
     if (image.truncated)
       return res.status(413).send({ error: 'File too large' });
-    var link = `data/${uuid()}.${type}`;
-    image.mv(`./${link}`, (err) => {
-      if (err) return res.status(500).send({ error: 'Error uploading file' });
-    });
+
+    const result = await db.query(`INSERT INTO images(image) VALUES($1) RETURNING iid`, [
+      data,
+    ]);
+    // console.log(result.rows[0])
+    var link = `images/${result.rows[0].iid}`; //.${type}
+
+    // image.mv(`./${link}`, (err) => {
+    //   if (err) return res.status(500).send({ error: 'Error uploading file' });
+    // });
     res.send({ payload: link });
   } catch (err) {
     console.log(err);
