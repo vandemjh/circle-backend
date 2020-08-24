c52a801de6c7e8aef17bc6475c6721b4a1bef0e7
vandemjh <jack.vandem@gmail.com>
2020-08-24 16:28:47 -0400

    added volume sharing for db backups

--- start-database.sh
+++ start-database.sh
@@ -1,1 +1,8 @@
-docker run --name postgres -e POSTGRES_PASSWORD=password -e POSTGRES_USER=dbuser -e POSTGRES_DB=db -p 5432:5432 -d postgres
+docker run --name postgres \
+  -e POSTGRES_PASSWORD=password \
+  -e POSTGRES_USER=dbuser \
+  -e POSTGRES_DB=db \
+  -e PGDATA=/var/lib/postgresql/data \
+  -v ~/database:/var/lib/postgresql/data \
+  -p 5432:5432 \
+  -d postgres
