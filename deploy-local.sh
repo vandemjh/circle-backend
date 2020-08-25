5f5ada4051074f27c7d0c4564ed0c93c6c55d526
vandemjh <jack.vandem@gmail.com>
2020-08-24 20:58:13 -0400

    fixed the deploy script

--- deploy-local.sh
+++ deploy-local.sh
@@ -1,6 +1,6 @@
-ssh -T pi@circle.local << EOSSH
-sudo docker run \
-  --name circle-backend \
-  -p 3000:3000 \
-  -d $(docker build -q .)
+# sudo docker rm circle-backend
+ssh -T pi@circle-backend.local << EOSSH
+cd ~/circle-backend
+sudo docker stop circle-backend
+sudo docker run --name circle-backend -p 3000:3000 -d \$(sudo docker build -q .)
 EOSSH
