326cd4df4f7522b1b2c7d7cfb9f4b77a68eb5dd5
vandemjh <jack.vandem@gmail.com>
2020-09-10 16:06:18 -0400

    draft of compression

--- routes/images.js
+++ routes/images.js
@@ -1,23 +1,30 @@
 const Router = require('express-promise-router');
 const db = require('../db/access');
-// const btoa = require('btoa');
+// const imagemin = require('imagemin');
+// const imageminJpegtran = require('imagemin-jpegtran');
+// const imageminPngquant = require('imagemin-pngquant');
 const fs = require('fs');
 const router = new Router();
 const express = require('express');
 
 module.exports = router;
 
-router.use('/data', express.static('./data'));
-
 router.get('/:iid', async (req, res) => {
   const iid = req.params.iid;
   const result = await db.query(`SELECT image FROM images WHERE iid = $1`, [
     iid,
   ]);
-  //   console.log(result)
   if (!result || result.rows.length == 0)
     return res.status(404).send({ error: 'No such image.' });
   const image = result.rows[0].image;
+  // imagemin(['images/*.{jpg,png}'], {
+  //   plugins: [
+  //     // These are lossy
+  //     imageminJpegtran(),
+  //     imageminPngquant(),
+  //   ],
+  // });
+
   //   var binary = btoa(String.fromCharCode.apply(null, new Uint8Array(image)));
 
   //   console.log(image);
