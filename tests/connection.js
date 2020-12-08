0c9187957467c7ccf21b5a837ce414ebedf15ba2
vandemjh <jack@jackv.dev>
2020-12-08 12:23:10 -0500

    Fixed test connection

--- tests/connection.js
+++ tests/connection.js
@@ -2,11 +2,10 @@ require('dotenv').config();
 const { Pool } = require('pg');
 const pool = new Pool();
 
-
-try {
-	pool.query("CALL NOW()");
-} catch(err) {
+pool.query("SELECT NOW();").then((resp) => {
+	console.log("Success " + resp.rows[0]);
+	return true;
+}).catch((err) => {		
 	console.log(err);
-	return false;
-}
-return true;
+});
+return false;
