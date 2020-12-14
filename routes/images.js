6d24f40b95ea751ee8758a684f9d82c6f9957378
vandemjh <jack@jackv.dev>
2020-12-13 19:48:47 -0500

    Added endpoint support for minified images

--- routes/images.js
+++ routes/images.js
@@ -4,19 +4,18 @@ const router = new Router();
 
 module.exports = router;
 
-router.get('/:iid', async (req, res) => {
+router.get('/:minified/:iid', async (req, res) => {
   const iid = req.params.iid;
+  const minified = req.params.minified === 'minified' ? 'minified' : 'image';
   const result = await db.query(
-    `SELECT ${
-      process.env.MINIFIED === 'true' ? 'minified' : 'image'
-    } FROM images WHERE iid = $1`,
-    [iid]
+    `SELECT ${minified} FROM images WHERE iid = $1`,
+    [
+      iid,
+    ]
   );
   if (!result || result.rows.length == 0)
     return res.status(404).send({ error: 'No such image.' });
   const image =
-    process.env.MINIFIED === 'true'
-      ? result.rows[0].minified
-      : result.rows[0].image;
+    minified === 'minified' ? result.rows[0].minified : result.rows[0].image;
   res.send(image);
 });
